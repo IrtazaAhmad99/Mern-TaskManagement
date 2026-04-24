b3ff8a910351f56fc1550b166d9cf3529294c258
Your Name <you@example.com>
2026-04-24 21:53:36 +0500

    task crud completed

---
+++ Backend/routes/taskRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const router = express.Router()
+
+const TaskController = require("../controllers/Task/TaskController.js")
+
+const {protectedRoutes} = require("../middlewares/authMiddleware")
+
+router.use(protectedRoutes)
+
+router.post("/create", TaskController.createTask)
+
+router.get("/getall", TaskController.getAllTask)
+
+router.put("/:id", TaskController.updateTask)
+
+router.delete("/delete/:id", TaskController.deleteTask)
+
+router.get("/single/:id", TaskController.getSingleTask)
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const connectdb = require("./DB/connectdb")
 const ENV = require("./Helper/ENV/environment")
 const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes")
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/task",taskRoutes)
 
 connectdb()
 app.get("/", (req, res) => {
