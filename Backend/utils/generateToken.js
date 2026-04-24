a158f76e75b9cae62714467d54f321449f7e91d8
Your Name <you@example.com>
2026-04-25 00:36:51 +0500

    admin system implemented completely

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
+const ENV = require("../Helper/ENV/environment.js")
 
 exports.generateToken = (user)=>{
 return jwt.sign(
     {id:user._id, role:user.role}, 
-    process.env.JWT_SECRET, 
+    ENV.JWT_SECRET, 
     {expiresIn:"7d"})
 }
