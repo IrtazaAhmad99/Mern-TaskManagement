8a5b896843f66be3c4baea2163f4ca23f283f291
Your Name <you@example.com>
2026-04-25 06:18:23 +0500

    tailwind css installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,11 @@ function App() {
  
 
   return (
-    <>
-      <h1>
-        hello world
+    <div className="p-10">
+      <h1 className="text-4xl text-green-500">
+        Tailwind is working
       </h1>
-    </>
+    </div>
   )
 }
 
